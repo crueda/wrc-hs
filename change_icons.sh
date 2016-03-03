61e4289a61930f80635d01b4a3ea202e459e0df6
Carlos Rueda Morales <crueda@gmail.com>
2016-03-03 10:58:55 +0100

    aa

--- change_icons.sh
+++ change_icons.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #-*- coding: UTF-8 -*-
 
-sed  -i  's/http:\/\/maps.google.com\/mapfiles\/kml\/pal4\/icon7.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/red-circle_maps.png/' mexico.kml
+sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/pal4\/icon7.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/red-circle_maps.png/' ./kml/mexico.kml
+sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/pal4\/icon54.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/grn-circle_maps.png/' ./kml/mexico.kml
+sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/pushpin\/ylw-pushpin.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/ylw-stars-lv.png/' ./kml/mexico.kml
+sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/mechanic.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/blu-stars-lv.png/' ./kml/mexico.kml
+sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/gas_stations.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/blu-blank-lv.png/' ./kml/mexico.kml
+sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/shaded_dot.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/wht-stars-lv.png/' ./kml/mexico.kml
+sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/placemark_circle_highlight.png/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/placemark_circle.png/' ./kml/mexico.kml
 
 
+
+
+sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/pal4\/icon7.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/red-circle_maps.png/' ./kml/mexico.kml
 
 
 
@@ -13,13 +22,13 @@ sed  -i  's/http:\/\/maps.google.com\/mapfiles\/kml\/pal4\/icon7.png/http:\/\/ma
 http://tancro.e-central.tv/grandmaster/markers/google-icons/mapfiles-kml-paddle.html
 -----
 
-rojo
+rojo - fin de tramo
 http://maps.google.com/mapfiles/kml/paddle/red-circle_maps.png
 
-verde
+verde - principio de tramo
 http://maps.google.com/mapfiles/kml/paddle/grn-circle_maps.png
 
-amarillo
+amarillo - marcas
 http://maps.google.com/mapfiles/kml/paddle/ylw-stars-lv.png
 
 service:
