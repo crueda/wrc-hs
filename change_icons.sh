6ff4479a3a61468c5a5be774d734f0ee7501e37e
Carlos Rueda Morales <crueda@gmail.com>
2016-03-03 10:24:47 +0100

    change icons

---
+++ change_icons.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#-*- coding: UTF-8 -*-
+
+sed  -i  's/http:\/\/maps.google.com\/mapfiles\/kml\/pal4\/icon7.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/red-circle_maps.png/' mexico.kml
+
+
+
+
+
+
+
+-----
+http://tancro.e-central.tv/grandmaster/markers/google-icons/mapfiles-kml-paddle.html
+-----
+
+rojo
+http://maps.google.com/mapfiles/kml/paddle/red-circle_maps.png
+
+verde
+http://maps.google.com/mapfiles/kml/paddle/grn-circle_maps.png
+
+amarillo
+http://maps.google.com/mapfiles/kml/paddle/ylw-stars-lv.png
+
+service:
+http://maps.google.com/mapfiles/kml/paddle/blu-stars-lv.png
+
+split:
+http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png
+
+radio point
+http://maps.google.com/mapfiles/kml/paddle/wht-stars-lv.png
+
+--
+blanco
+http://maps.google.com/mapfiles/kml/paddle/wht-circle-lv.png
