6ed5fe1e5a37881de719382085fa0f3451be12ff
Carlos Rueda Morales <crueda@gmail.com>
2016-07-29 08:19:25 +0200

    aa

--- change_icons.sh
+++ change_icons.sh
@@ -1,45 +1,9 @@
 #!/bin/bash
-#-*- coding: UTF-8 -*-
 
-sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/pal4\/icon7.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/red-circle_maps.png/' ./kml/mexico.kml
-sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/pal4\/icon54.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/grn-circle_maps.png/' ./kml/mexico.kml
-sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/pushpin\/ylw-pushpin.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/ylw-stars-lv.png/' ./kml/mexico.kml
-sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/mechanic.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/blu-stars-lv.png/' ./kml/mexico.kml
-sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/gas_stations.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/blu-blank-lv.png/' ./kml/mexico.kml
-sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/shaded_dot.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/wht-stars-lv.png/' ./kml/mexico.kml
-sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/placemark_circle_highlight.png/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/placemark_circle.png/' ./kml/mexico.kml
-
-
-
-
-sed -i 's/http:\/\/maps.google.com\/mapfiles\/kml\/pal4\/icon7.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/red-circle_maps.png/' ./kml/mexico.kml
-
-
-
-
-
------
-http://tancro.e-central.tv/grandmaster/markers/google-icons/mapfiles-kml-paddle.html
------
-
-rojo - fin de tramo
-http://maps.google.com/mapfiles/kml/paddle/red-circle_maps.png
-
-verde - principio de tramo
-http://maps.google.com/mapfiles/kml/paddle/grn-circle_maps.png
-
-amarillo - marcas
-http://maps.google.com/mapfiles/kml/paddle/ylw-stars-lv.png
-
-service:
-http://maps.google.com/mapfiles/kml/paddle/blu-stars-lv.png
-
-split:
-http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png
-
-radio point
-http://maps.google.com/mapfiles/kml/paddle/wht-stars-lv.png
-
---
-blanco
-http://maps.google.com/mapfiles/kml/paddle/wht-circle-lv.png
+sed  -i  's/http:\/\/maps.google.com\/mapfiles\/kml\/pal4\/icon7.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/red-circle_maps.png/g' $1
+sed  -i  's/http:\/\/maps.google.com\/mapfiles\/kml\/pal4\/icon54.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/grn-circle_maps.png/g' $1
+sed  -i  's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/mechanic.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/ylw-stars-lv.png/g' $1
+sed  -i  's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/gas_stations.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/ylw-circle-lv.png/g' $1
+sed  -i  's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/shaded_dot.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/red-stars-lv.png/g' $1
+sed  -i  's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/placemark_circle.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/wht-circle-lv.png/g' $1
+sed  -i  's/http:\/\/maps.google.com\/mapfiles\/kml\/shapes\/placemark_circle_highlight.png/http:\/\/maps.google.com\/mapfiles\/kml\/paddle\/wht-circle-lv.png/g' $1
